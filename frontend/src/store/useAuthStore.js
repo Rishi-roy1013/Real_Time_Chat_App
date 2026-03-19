a91ff28750ab27151163627e1f602e589b1273d2
rishi-roy1013 <rrishikesh768@gmail.com>
2026-03-19 16:13:04 +0530

    Tiny fix

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const SOCKET_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "https://chat-server-cqtf.onrender.com";
+const SOCKET_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "https://chat-server-cqtf.onrender.com/api";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
